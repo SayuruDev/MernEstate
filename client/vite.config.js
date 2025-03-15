7742cf9727447315b88c0a47e25e02c4fbb0922c
SayuruDev <mailtosayuru@gmail.com>
2025-03-15 14:51:44 -0400

    Section4 - SignUp Complete

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,13 @@ import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        secure: false,
+      },
+    },
+  },
   plugins: [tailwindcss(), react()],
 });

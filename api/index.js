41bb53a78b1c244c43adbfea989547a4481a315c
SayuruDev <mailtosayuru@gmail.com>
2025-03-15 12:51:08 -0400

    SignUp Page UI

--- api/index.js
+++ api/index.js
@@ -12,6 +12,16 @@ mongoose.connect(
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
+
 app.listen(3000, () => {
   console.log("server is runing on port 3000**!!!");
 });

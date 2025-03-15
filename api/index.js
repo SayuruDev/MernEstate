57a21620fee0b5ad2748d59d5f11c9899b3d859f
SayuruDev <mailtosayuru@gmail.com>
2025-03-14 21:39:59 -0400

    connect to backend and test POST method

--- api/index.js
+++ api/index.js
@@ -1,14 +1,17 @@
 import express from "express";
 import mongoose from "mongoose";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 const app = express();
+app.use(express.json());
 mongoose.connect(
   "mongodb+srv://sagara:sagara123@cluster0.blx4tkl.mongodb.net/mern-estate?retryWrites=true&w=majority&appName=Cluster0"
 );
 
-app.listen(3000, () => {
-  console.log("server is runing on port 3000");
-});
-
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
+app.listen(3000, () => {
+  console.log("server is runing on port 3000**!!!");
+});

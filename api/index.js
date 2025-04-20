e4eab07785af6d51df5690700b3cd53c264753ac
SayuruDev <mailtosayuru@gmail.com>
2025-04-20 15:14:27 -0400

    7.Connect to the Database

--- api/index.js
+++ api/index.js
@@ -1,29 +1,19 @@
 import express from "express";
 import mongoose from "mongoose";
-import userRouter from "./routes/user.route.js";
-import authRouter from "./routes/auth.route.js";
-import listingRouter from "./routes/listing.route.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB!!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
-app.use(express.json());
-mongoose.connect(
-  "mongodb+srv://sagara:sagara123@cluster0.blx4tkl.mongodb.net/mern-estate?retryWrites=true&w=majority&appName=Cluster0"
-);
-
-app.use("/api/user", userRouter);
-app.use("/api/auth", authRouter);
-app.use("/api/listing", listingRouter);
-
-app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || "Internal Server Error";
-  return res.status(statusCode).json({
-    success: false,
-    statusCode,
-    message,
-  });
-});
 
 app.listen(3000, () => {
-  console.log("server is runing on port 3000!!!!");
+  console.log("Server is running on port 3000!");
 });

dd73ea6551ef227b715344216d05c17a8ba4ee6e
SayuruDev <mailtosayuru@gmail.com>
2025-04-20 20:50:58 -0400

    16-Add redux Toolkit+ Checked Login with React Tools

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,12 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { persistor, store } from "./redux/store.js";
+import "./index.css";
+import { store } from "./redux/store.js";
 import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
